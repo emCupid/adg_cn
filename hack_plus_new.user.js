e263bdeea0b993986cae8ca87e95d25305f0cac1
ilvjyw <ilvjyw@163.com>
2022-03-16 22:07:46 +0800

    u

--- hack_plus_new.user.js
+++ hack_plus_new.user.js
@@ -259,6 +259,7 @@ if (hackplus_whitelistJSON == "{}") {
 function Fuck_ADV(){
     [].forEach.call(document.querySelectorAll('a[target] img:not([src*="avatar"]),a[style] img:not([src*="avatar"]),a[onclick] img:not([src*="avatar"]),a[href*="javascript"] img:not([src*="avatar"]),a[rel*="nofollow"] img:not([src*="avatar"]),a img[style*="display"][style*="block"],a:not([href*="' + getDoamin.split('.')[0] + '."]):not([href^="/"]) img'), function (Nodeitem) {
         Fuck_removeAD(Nodeitem, 580, 1800, 40, 150, 2);
+        Fuck_removeAD(Nodeitem, 350, 400, 100, 150, 2);//
         Fuck_removeAD(Nodeitem, 40, 150, 300, 650, 2, "#08E")
     });
     [].forEach.call(document.getElementsByTagName('iframe'), function (Nodeitem) {
