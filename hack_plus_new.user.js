4dbcf90b32153fad347d4e5c576d5866a5727539
ilvjyw <ilvjyw@163.com>
2022-03-16 21:13:52 +0800

    u

--- hack_plus_new.user.js
+++ hack_plus_new.user.js
@@ -172,6 +172,9 @@ function Fuck_removeAD(Item, MinWidth, MaxWidth, MinHeight, MaxHeight, RmoveFun,
                 console.log('%c[Remove AD] ✂%o', logCss, Item.src || Item);
                 break;
         }
+    }else if((Item.offsetWidth && Item.offsetHeight) || (Item.naturalWidth  && Item.naturalHeight)){
+    
+    console.log('%c[log offset] ✂%O',logCss,'offsetWidth:' ,Item.offsetWidth||Item.naturalWidth,'offsetHeight:',Item.offsetHeight||Item.naturalHeight ,Item.src || Item);
     }
 }
 var getDoamin = window._getMainHost || window.location.host,
