dd3b8f761a820cfae0270c155100398d1f50d487
emCupid <m@x-boy.cn>
2022-10-05 04:29:53 +0800

    ,

--- hack_plus.user.js
+++ hack_plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         朝朝暮暮plus
-// @version      1.31.1004
+// @version      1.31.1005
 // @author       汝莫舞
 // @description  一些浏览器增强功能及辅助移除广告【Ctrl+↑脚本设置】
 // @homepageURL  https://github.com/emCupid/adg_cn
